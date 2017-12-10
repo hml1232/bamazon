2ff9c01aa9aa832930990709965942ed9ee09641
Haley Lock <haleylock@Haleys-MacBook-Air.local>
2017-12-10 10:54:57 -0500

    trial

--- bamazon.js
+++ bamazon.js
@@ -1,134 +1,81 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var colors = require('colors');
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "",
-    database: "Bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "safety",
+  database: "bamazon_db"
 });
 
-
-// Connects SQL DB with Node
-connection.connect(function (error) {
-    if (error) throw error;
-    initialRun();
+connection.connect(function(err, rsp){
+  if(err) throw err;
+  displayInv();
 });
 
-// Display and perform actions
-function initialRun() {
-    var query = "SELECT * FROM products";
-
-    // Executes query
-    connection.query(query, function (error, result) {
-        console.log(chalk.green.bold("\nCurrent Products in Bamazon"));
-        console.log(chalk.green.bold(dashes));
-
-        for (var i = 0; i < result.length; i++) {
-            console.log(chalk.bold(result[i].id + " | ") + result[i].product_name + " ---- $" + chalk.red(result[i].price));
-        }
-
-        console.log(chalk.green(dashes));
-        custPurchase();
-    });
-};
-
-// Customer purchase function
-function custPurchase() {
-    // Prompts that ask if the customer wants to make a purchase
-    inquirer
-        .prompt({
-            name: "options",
-            type: "list",
-            message: "Would you like to make a purchase?: ",
-            choices: [
-                "Yes",
-                "No"
-            ]
-        })
-        .then(function (answer) {
-            // Executes when yes is selected
-            if (answer.options === "Yes") {
-                // Prompts that ask what item to buy and the number to purchase
-                inquirer
-                    .prompt([
-                            {
-                                name: "buyID",
-                                type: "input",
-                                message: "Please enter the ID of the product you would like to buy: ",
-                                validate: function (value) {
-                                    if (isNaN(value) === false) {
-                                            return true;
-                                    }
-                                    return false;
-                                }
-                            },
-                            {
-                                name: "numUnits",
-                                type: "input",
-                                message: "How many would you like to buy?: ",
-                                validate: function (value) {
-                                    if (isNaN(value) === false) {
-                                            return true;
-                                    }
-                                    return false;
-                                }
-                            }
-                    ])
-                    .then(function (answer) {
-                        connection.query("SELECT * FROM products WHERE id = ?", [answer.buyID], function (error, result) {
-                            var currNumItems = result[0].stock_quantity;
-                            var currItemName = result[0].product_name;
-                            var currPrice = result[0].price;
-
-                            purchaseItem(currItemName, answer.buyID, answer.numUnits, currNumItems, currPrice);
-                        })
-
-                    })
-            }
-            // Terminates the connection since it is not needed anymore 
-            else {
-                console.log(chalk.green(dashes));
-                console.log(chalk.blue("Thank you for your time. Goodbye."));
-                connection.end();
-            }
-    })
+function displayInv(){
+  connection.query("SELECT * FROM products", function(err, resp){
+    for(var i=0; i < resp.length; i++){
+      console.log("ID: "+ resp[i].unique_id +" || Item: "+ resp[i].item_name +" || Price: $"+ resp[i].price);
+    }
+    runPOS();
+  });
 }
 
-// Function that takes the selected item and subtracts it from the quantity in the db
-function purchaseItem(name, itemNum, units, currUnits, currPrice) {
-    var query = "UPDATE products SET ? WHERE ?";
-    var newNumUnits = currUnits - units;
-    var totalPrice = currPrice * units;
-
-    if (newNumUnits >= 0) {
-        connection.query(query, [
-            {
-                stock_quantity: newNumUnits
-            },
-            {
-                id: itemNum
+function runPOS(){
+  inquirer.prompt([
+  {
+    name: "choice",
+    type: "input",
+    message: "Select the ID of the Item you would like:",
+    validate: function(value) {
+      if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+    }
+  },
+  {
+    name: "quantity",
+    type: "input",
+    message: "How many would you like:",
+    validate: function(value) {
+      if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+    }
+  }
+  ]).then(function(answer){
+    connection.query("SELECT * FROM products WHERE unique_id = ?", [answer.choice], function(err,res){
+      if(err) throw err;
+      if(parseInt(res[0].stock) < parseInt(answer.quantity)){
+        console.log("I'm sorry, there are only " + res[0].stock + " of that item left.");
+        console.log("Please try again with a different quantity.");
+        runPOS();
+      }
+      else {
+        connection.query("UPDATE products SET stock = ? WHERE unique_id= ?", [(res[0].stock - answer.quantity), answer.choice], function(err, rsp){
+          if(err) throw err;
+          console.log("$"+ parseFloat(res[0].price) * parseInt(answer.quantity) +" has been deducted from your account.");
+          inquirer.prompt({
+            name: "keepShopping",
+            type: "confirm",
+            message: "Would you like to keep shopping?"
+          }).then(function(answer){
+            if(answer.keepShopping){
+              runPOS();
             }
-        ], function (error) {
-            console.log(chalk.green(dashes));
-            console.log(chalk.green("Thanks for the purchase. You have just purchased " + units + " " + name + " for a total of $" + totalPrice + ". There are now " + newNumUnits + " " + name + " remaining."));
-            console.log(chalk.green(dashes));
+            else {
+              console.log("Thank you for shopping with Bamazon! Have an adequate day.");
+              connection.end;
+            }
+          });
+        });
+      }
 
-            custPurchase();
-        })
-    }
-    else {
-        console.log(chalk.green(dashes));
-        console.log(chalk.red("Insufficient quantity! Purchasing of this item is not currently available. Purchasing will be available once product is back in stock."));
-        console.log(chalk.green(dashes));
+    });
 
-        custPurchase();
-    }
+  });
 }
--- schema.sql
+++ schema.sql
@@ -1,67 +1,15 @@
-DROP DATABASE IF EXISTS Bamazon;
-CREATE DATABASE Bamazon;
+DROP DATABASE IF EXISTS bamazon_db;
 
-USE Bamazon;
+CREATE DATABASE bamazon_db;
 
+USE bamazon_db;
 
--- Creates table products
-CREATE TABLE IF NOT EXISTS products (
-	id INTEGER(11) AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(40) NOT NULL,
-    department_name VARCHAR(30),
-    price FLOAT(11) NOT NULL,
-    stock_quantity INTEGER(200) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE products(
+	unique_id INT(10) NOT NULL AUTO_INCREMENT,
+	item_name VARCHAR(60) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock INT(10) NULL,
+	department VARCHAR(20) NULL,
+	PRIMARY KEY(id)
 );
-
--- First product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Swimming Goggles", "Sports", 20, 24);
-
--- Second product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Surface Book", "Electronics", 2500, 9);
-
--- Third product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Arteza Woodless Watercolor Pencils", "Supplies", 19.99, 19);
-
--- Fourth product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Motorola Nexus 6 Unlocked", "Electronics", 229.99, 3);
-
--- Fifth product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("LUENX Aviator Sunglasses", "Fashion", 15.29, 49);
-
--- Sixth product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("ONSON Micro USB Cable", "Electronics", 9.74, 12);
-
--- Seventh product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Titanium Bonded Scissors", "Supplies", 6.30, 99);
-
--- Eighth product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("No Show Liner Socks", "Fashion", 9.99, 100);
-
--- Ninth product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Baby Boy Clothes Long", "Baby Clothes", 8, 22);
-
--- Tenth product
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Kindle Fire", "Electronics", 160, 101);
-
--- Eleventh product. Demo of 0 stock_quantity.
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("MacBook Pro", "Electronics", 2500, 0);
-
--- Select all products in database.
-SELECT * FROM products;
- 
- 
- 
-
 
